5494822976eecfb08fa64176bc8b00b70987e6bf
diahlokamp <pdiahloka@student.ciputra.ac.id>
2026-04-17 11:08:24 +0700

    update 17-04-2026

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,11 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use softDeletes;
     public function product_category():BelongsTo
     {
         return $this->belongsTo(ProductCategory::class, 'category_id');
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,9 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
-// Route::get('/shop', function () {
-//     return view('shop');
-// })->name('shop');
+
 Route::get('/shop', [StoreController::class, 'show'])->name('shop');
-// Route::get('/shop/{id}', [StoreController::class, 'show'])->name('
+
 
 Route::get('/home', [HomeController::class, 'show'])->name('home');
 
@@ -33,3 +31,12 @@ Route::get('/home/sum', [FirstController::class, 'sum'])->name('home.sum');
 
 Route::get('/home/multiply/{param1}/{param2?}', [FirstController::class, 'multiply'])->name('home.multiply');
 
+Route::get('/product/insert-form', [StoreController::class, 'product_insert_form'])->name('product_insert_form');
+
+Route::post('/product/insert', [StoreController::class, 'insert_product'])->name('insert_product');
+
+Route::get('/product/edit/{product_id}', [StoreController::class, 'product_edit_form'])->name('product_edit_form');
+
+Route::put('/product/update/{product_id}', [StoreController::class, 'update_product'])->name('update_product');
+
+Route::delete('/product/delete/{product_id}', [StoreController::class, 'delete_product'])->name('delete_product');
